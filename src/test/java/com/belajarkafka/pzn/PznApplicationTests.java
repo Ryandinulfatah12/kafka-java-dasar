3ab644edfe4d5d8c64ab5a14e5c2817dc065836a
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2024-02-28 13:43:53 +0700

    init

---
+++ src/test/java/com/belajarkafka/pzn/PznApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajarkafka.pzn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PznApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
